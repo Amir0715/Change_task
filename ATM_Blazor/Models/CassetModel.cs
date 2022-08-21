046ca8733bcb5059fd4a3e80b3ecea3f6732a037
Kamolov Amir <kamolov.amir2000@yandex.ru>
2022-08-21 21:44:59 +0300

    [+] Решение задачи

---
+++ ATM_Blazor/Models/CassetModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ATM_Blazor.Models;
+
+public class CassetModel : ICloneable
+{
+    public int id { get; set; }
+
+    [Required]
+    public int nominal { get; set; }
+    [Required]
+    [Range(0, Int32.MaxValue, ErrorMessage = "Кол-во купюр должно быть >= 0")]
+    public int countBanknotes { get; set; }
+    [Range(typeof(bool), "true", "true")]
+    public bool faulty { get; set; } = false;
+
+    public object Clone()
+    {
+        return new CassetModel
+        {
+            id = this.id,
+            nominal = this.nominal,
+            faulty = this.faulty,
+            countBanknotes = this.countBanknotes
+        };
+    }
+}
